ad867e4c956deeb33babcf7634f5e6d6a9df16c7
Mimo <Mimo@DESKTOP-VCI4DS3>
2017-04-17 20:22:51 +0300

    Working wpf.

--- InitialData/Clients.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO Clients VALUES
-('Economedia', 'BG1221333445', 1, '20 Ivan Vazov Str.', 'Koicho Koichev', '+359 555 555 555', 'true'),
-('GD Publishing', 'BG3221453423', 2, '12 Zelena Livada Str.', 'Blagoi Blagoev', '+359 444 444 444', 'true'),
-('DD News', 'BG3547845423', 3, '55 Ravno Pole Str.', 'Temenujka Zumbuleva', '+359 777 555 333', 'true');
